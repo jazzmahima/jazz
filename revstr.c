92e1350370a4075669624d11e4ecb16639a892a4
Jasmine.R <keziajazzz@gmail.com>
2017-06-27 12:04:12 +0530

    Update revstr.c

--- revstr.c
+++ revstr.c
@@ -2,21 +2,22 @@
 #include<conio.h>
 void main()
 {
-   int i, j, k;
+   int i,j,k;
    char str[100];
    char rev[100];
+   clrscr();
    printf("Enter a string\t");
-   scanf("%s", str);
-   printf("The original string is %s", str);
-   for(i = 0; str[i] != '\0'; i++);
+   scanf("%s",str);
+   printf("The original string is %s",str);
+   for(i=0;str[i]!='\0';i++);
    {
-      k = i-1;
+     k=i-1;
    }
-   for(j = 0; j <= i-1; j++)
+   for(j=0;j<=i-1;j++)
    {
-      rev[j] = str[k];
+      rev[j]=str[k];
       k--;
    }
-   printf("The reverse string is %s", rev);
+   printf("The reverse string is %s",rev);
    getch();
 }
